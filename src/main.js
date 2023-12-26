62fda0a5d73d45b26bbd7b69e86cf1c23f3e4f70
Andres Gutierrez <agutierrez.codes@gmail.com>
2023-12-27 00:51:02 +0100

    Implement mobile active state

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
+import tippy from 'tippy.js'
+import 'tippy.js/dist/tippy.css'
 import './css/styles.css'
+import Alpine from 'alpinejs'
+
+window.tippy = tippy
+
+Alpine.data('tooltip', () => ({
+  isOpen: false,
+
+  trigger: {
+    ['@click']() {
+      this.isOpen = !this.isOpen
+    }
+  }, 
+  close() {
+    this.isOpen = false
+  }
+
+}))
+
+window.Alpine = Alpine
+
+Alpine.start()
